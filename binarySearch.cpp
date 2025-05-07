8012d4c77d96f454e6b38e7cf6d139ae7a7ef8bf
riswandaP <riswandaputra03@gmail.com>
2025-05-07 20:01:02 +0700

    membuat comand "masukkan elemen array" pada void input

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -21,3 +21,15 @@ void input()
             cout << "\nJumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
+
+    cout << "==============================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "==============================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> elemen[i];
+    }
+}
+
