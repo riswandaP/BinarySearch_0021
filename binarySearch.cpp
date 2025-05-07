2ab40cb2088179aaf1f90a23ebc2e6d131caaba7
riswandaP <riswandaputra03@gmail.com>
2025-05-07 20:04:22 +0700

    membuat void binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -67,3 +67,19 @@ void display()
     cout << endl;
 }
 
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n==============================\n";
+        cout << "Pencarian Binary Search\n";
+        cout << "==============================\n";
+
+        int item;
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> item;
+
+        int low = 0;
+        int high = n - 1;
+
